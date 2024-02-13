5ffaefea06260acce3658a9fddc1a21775c39906
DmitryGusev91 <82973927+DmitryGusev91@users.noreply.github.com>
2024-02-13 17:26:05 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -32,6 +32,13 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use("/auth", authRouter);
 app.use("/my-hotels", myHotelsRouter);
 app.use("/hotels", hotelsRouter);
