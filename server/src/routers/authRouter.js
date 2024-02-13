043446c6931a81b5d1f39631c5e793c590a975f9
DmitryGusev91 <82973927+DmitryGusev91@users.noreply.github.com>
2024-02-13 21:47:53 +0200

    Update authRouter.js

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -101,6 +101,7 @@ router.get("/validate-token", verifyToken, (req, res) => {
 router.post("/logout", (req, res) => {
   res.cookie("auth_token", "", {
     expires: new Date(0),
+    httpOnly: true,
   });
   res.send();
 });
